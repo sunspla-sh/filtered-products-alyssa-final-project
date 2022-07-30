088c749ad89838940051ac16803f092d144013e4
jack <jrfisch95@gmail.com>
2022-07-30 09:51:45 -0400

    add additional comments to generate-map-of-valid-images.js and make-new-product-data.js

--- generate-map-of-valid-images.js
+++ generate-map-of-valid-images.js
@@ -1,19 +1,64 @@
+//our product data which we will check for image link validity
 const productArray = require('./product-data.json');
+
+/**
+ * our library for making http requests
+ * (alternatively we could use fetch as it is now
+ * native to Node.js if you're using v18 or later)
+ */
 const axios = require('axios');
+
+/**
+ * the fs library allows us to write data to new files
+ * so we will use this to generate a new JSON file
+ */
 const fs = require('fs');
 
+/**
+ * Event emitter will help us keep track of how many 
+ * image links we've checked for validity so far
+ */
 const EventEmitter = require('node:events');
 
+/**
+ * Extended the event emitter class in case we
+ * needed to add some custom methods or additional
+ * properties, but it turns out we didn't need them
+ */
 class MyEmitter extends EventEmitter {}
 
+/**
+ * Make a new instance of our EventEmitter
+ */
 const myEmitter = new MyEmitter();
 
+/**
+ * find out how many product image links we will
+ * need to check for validity
+ */
 let numProducts = productArray.length;
 
+/**
+ * create a variable which we will decrement by 1 after
+ * each image link check
+ */
 let imagesRemainingToCheck = numProducts;
 
+/**
+ * create a new array with a length equal to our total
+ * number of image links
+ */
 const validImageArray = new Array(numProducts);
 
+/**
+ * set up an event handler on our EventEmitter so that
+ * whenever it detects an 'imageResult' event it will
+ * decrement the imagesRemainingToCheck by 1, log the
+ * number of imagesRemainingToCheck along with the status
+ * code of the most recent image check, and write our
+ * validImageArray to a new file if there's no more
+ * images remaining to check
+ */
 myEmitter.on('imageResult', (statusCode) => {
   console.log(--imagesRemainingToCheck, statusCode);
   if(imagesRemainingToCheck === 0) {
@@ -22,8 +67,22 @@ myEmitter.on('imageResult', (statusCode) => {
   }
 });
 
+
+/**
+ * Here we loop through all of our product data and set up
+ * an HTTP request for each product image link with 1000 milliseconds
+ * (1 second) between each request so that we don't clobber their
+ * servers.
+ */
 for(let i = 0; i < numProducts; i++){
   setTimeout(() => {
+
+    /**
+     * After getting a valid response or error, we add the result
+     * to our validImageArray and emit an 'imageResult' event, which
+     * triggers the 'imageResult' event handler and associated callback function
+     * that we previously attached to our EventEmitter
+     */
     axios.get(productArray[i].image_link)
       .then(response => {
         validImageArray[i] = response.status;
@@ -35,6 +94,7 @@ for(let i = 0; i < numProducts; i++){
         myError = err.code;
         myEmitter.emit('imageResult', myError);
       })
+      
   }, 1000 * i);
 }
 
--- make-new-product-data.js
+++ make-new-product-data.js
@@ -1,7 +1,18 @@
+//require the native fs library which we will use to write to a new file
 const fs = require('fs');
+/**
+ * require our product data and valid image data which we will
+ * use to construct an array of products with valid images
+ */
 const productData = require('./product-data.json');
 const validImageData = require('./validImageArray.json');
 
+/**
+ * filter through our product data and only include the products
+ * in the new filteredArray if their images previously returned
+ * HTTP status code 200, which we can determine by checking our
+ * array from the validImageData.json file
+ */
 const filteredArray = productData.filter((product, index) => {
   if(validImageData[index] === 200){
     return true;
@@ -10,6 +21,10 @@ const filteredArray = productData.filter((product, index) => {
   }
 });
 
+/**
+ * write the array of products with valid image links
+ * to a new file called 'filteredProductData.json'
+ */
 let data = JSON.stringify(filteredArray, null, 2);
 fs.writeFileSync('filteredProductData.json', data);    
 
